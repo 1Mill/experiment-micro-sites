81d626732283b06487b22645fa3ce04a31dc0d27
1Mill <erik.ekberg@outlook.com>
2022-02-19 10:51:58 -0800

    Clean up default vue files

--- src/blue/src/App.vue
+++ src/blue/src/App.vue
@@ -1,31 +1,9 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+	<router-view/>
 </template>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  background-color: blue;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+	body {
+		border: 3px dashed blue;
+		padding: 1rem;
+	}
 </style>
