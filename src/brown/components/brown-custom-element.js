74c437f796e0e73eea14adcc01a0f35f83561722
1Mill <erik.ekberg@outlook.com>
2022-02-19 15:07:55 -0800

    Use microbundler

--- src/brown/components/brown-custom-element.js
+++ src/brown/components/brown-custom-element.js
@@ -2,6 +2,6 @@ export class BrownCustomElement extends HTMLElement {
 	connectedCallback() { this.render() }
 
 	render() {
-		this.innerHTML = 'Hello from Brown Custom Element render function'
+		this.innerHTML = 'Hello from Brown Custom Element render function using Microbundler!'
 	}
 }
