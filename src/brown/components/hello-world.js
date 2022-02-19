f133a3a384a34db1b035540cb8dfb22889f8720b
1Mill <erik.ekberg@outlook.com>
2022-02-19 14:13:51 -0800

    Render hello world from js file

---
+++ src/brown/components/hello-world.js
@@ -0,0 +1,12 @@
+export const helloWorld = () => {
+	return `
+		<h1>Hello world from BROWN hello-world component</h1>
+		<p>This is a multiline component</p>
+		<ul>
+			<li>Hello</li>
+			<li>World</li>
+			<li>from</li>
+			<li>BROWN</li>
+		</ul>
+	`
+}
--- src/brown/index.js
+++ src/brown/index.js
@@ -1,5 +1,6 @@
 import express, { Router } from 'express'
 import morgan from 'morgan'
+import { helloWorld } from './components/hello-world.js'
 
 const BASE_URL = Object.freeze('/brown')
 
@@ -11,6 +12,7 @@ app.use(morgan('dev'))
 // Namespaced routes (e.g. /aqua/dist/...)
 const routes = Router()
 routes.get('/', (req, res) => { res.send('<h1 style="border: 3px dashed brown;">Hello World from BROWN endpoint</h1>') })
+routes.get('/hello-world', (req, res) => { res.send(helloWorld()) })
 app.use(BASE_URL, routes)
 
 app.listen(process.env.PORT, process.env.HOST, () => {
