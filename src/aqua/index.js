628a26b007e59405d58583f5d002abb9008b21e2
1Mill <erik.ekberg@outlook.com>
2022-02-19 13:12:56 -0800

    Namespaces aqua routes to /aqua/...

--- src/aqua/index.js
+++ src/aqua/index.js
@@ -1,13 +1,19 @@
-import express from 'express'
+import express, { Router } from 'express'
 import morgan from 'morgan'
+
+const BASE_URL = Object.freeze('/aqua')
 
 const app = express()
 
+// Plugins
 app.use(morgan('dev'))
 
-app.get('/', (req, res) => {
-	res.send('Hello world!')
-})
+// Namespaced routes (e.g. /aqua/dist/...)
+const routes = Router()
+routes.get('/', (req, res) => { res.send('Hello world!') })
+routes.use('/components', express.static('./components'))
+routes.use('/dist', express.static('./dist'))
+app.use(BASE_URL, routes)
 
 app.listen(process.env.PORT, process.env.HOST, () => {
 	console.info('Listening on port:', process.env.PORT)
