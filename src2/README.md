4ed848a727cccef02e432ac81b364b4c7aff0e28
1Mill <erik.ekberg@outlook.com>
2022-02-24 20:47:38 -0800

    Scaffold container service

---
+++ src2/README.md
@@ -0,0 +1,5 @@
+# References
+
+* <https://demo.microfrontends.com/>
+* <https://github.com/micro-frontends-demo>
+* <https://martinfowler.com/articles/micro-frontends.html>
---
+++ src2/container/src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import { Fragment } from 'react'
+
+const App = () => (
+	<h1>Hello from CONTAINER service</h1>
+)
+ReactDOM.render(<App/>, document.getElementById('root'))
